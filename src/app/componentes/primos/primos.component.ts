9cbcba349a7d92c29bace41312e73f4d84ab40b2
nachomonllor <nachomonllorc@gmail.com>
2020-04-28 03:38:32 -0300

    agrego timer a juego de numeros primos pero no reinicia al terminar el tiempo

--- src/app/componentes/primos/primos.component.ts
+++ src/app/componentes/primos/primos.component.ts
@@ -13,8 +13,38 @@ export class PrimosComponent implements OnInit {
     this.primos.IniciarJuego();
   }
 
-  ngOnInit(): void {
+  _timer:any;
+
+  ngOnInit() {
     this.primos.IniciarJuego();
-  }
+      this._timer = setInterval(() => this.contador(), 1000);
+    }
+
+    contador(){
+      //clearInterval(myVar);
+      this.primos.reloj--;
+      if(this.primos.reloj<=0){
+        clearInterval(this._timer);
+        //alert("Se te acabo el tiempo");
+        //this.puntos=this.puntos-10;
+        this.primos.reloj=30;
+        this.primos.maximo_puntaje = this.primos.puntos;
+        this.primos.puntos = 0;
+        this.primos.resetearColorBotones();
+        this.primos.nivel = 1;
+        
+        //this.primos.limite = 10; //limite de los numeros aleatorios
+  
+        //this.NuevoJuego();
+  
+        //this.nuevoJuego.pasarSiguiente();
+  
+       
+  
+      }
+    // this.pasarSiguiente();
+    }
+ 
+    
 
 }
